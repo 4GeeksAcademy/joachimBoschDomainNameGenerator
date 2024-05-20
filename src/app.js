db2276cbd97a1fe2730228527235a3d76c05d23f
BoschJ <bosch.joachim@outlook.com>
2024-05-20 21:07:41 +0200

    Updated with FOR loops

--- src/app.js
+++ src/app.js
@@ -11,15 +11,31 @@ function domainNameGenerator() {
   let adj = ['great', 'notsogreat', 'big', 'lonely', 'amazing'];
   let noun = ['jogger', 'diver', 'bear', 'pony', 'statue'];
   let domain = ['.com', '.net', '.fr', '.be', '.gov']
-  let page = ['/myfirstadventurehtml', '/alwayshungry.html', '/bruges_is_the_nicest_city_of_northern_europe.html', '/index.html']
+  let page = ['/myfirstadventure.html', '/alwayshungry.html', '/bruges_is_the_nicest_city_of_northern_europe.html', '/index.html']
 
-  let part1 = Math.floor(Math.random() * pronoun.length);
+  /* let part1 = Math.floor(Math.random() * pronoun.length);
   let part2 = Math.floor(Math.random() * adj.length);
   let part3 = Math.floor(Math.random() * noun.length);
   let part4 = Math.floor(Math.random() * domain.length);
   let part5 = Math.floor(Math.random() * page.length);
 
-  return `https://${pronoun[part1]}${adj[part2]}${noun[part3]}${domain[part4]}${page[part5]}`
+  return `https://${pronoun[part1]}${adj[part2]}${noun[part3]}${domain[part4]}${page[part5]}` */
+
+  let fullDomainName = [];
+
+  for (let i = 0; i<pronoun.length; i++) {
+    for (let j = 0; j<adj.length; j++) {
+      for (let k = 0; k<noun.length; k++) {
+        for (let l = 0; l<domain.length; l++) {
+          for (let m = 0; m<page.length; m++) {
+            fullDomainName.push(pronoun[i] + adj[j] + noun[k] + domain[l] + page[m]);
+          }
+        }
+      }
+    }
+  }
+
+  return fullDomainName;
 };
 
 console.log(domainNameGenerator())
